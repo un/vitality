98e9641c4dee6f734b15a197928bdc7dbfcb3b93
McPizza0 <gh@l1.io>
2025-02-05 21:50:38 +0100

    fix circular dep

--- packages/auth/auth.ts
+++ packages/auth/auth.ts
@@ -1,24 +1,24 @@
-import { passkeyClient, usernameClient } from 'better-auth/client/plugins';
-import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { admin, username } from 'better-auth/plugins';
-import { createAuthClient } from 'better-auth/react';
-import { betterAuth } from 'better-auth';
+import { passkeyClient, usernameClient } from "better-auth/client/plugins";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { admin, username } from "better-auth/plugins";
+import { createAuthClient } from "better-auth/react";
+import { betterAuth } from "better-auth";
 
-import { passkey } from 'better-auth/plugins/passkey';
-import { db } from '@flow/database';
-import { UserRoles } from './types';
+import { passkey } from "better-auth/plugins/passkey";
+import { db } from "@augment/database";
+import { UserRoles } from "@augment/utils/types/auth";
 
 export const auth = betterAuth({
   baseURL: process.env.WEB_BASE_URL,
   database: drizzleAdapter(db, {
-    provider: 'pg'
+    provider: "pg",
   }),
-  user: { modelName: 'users' },
-  session: { modelName: 'sessions' },
-  account: { modelName: 'accounts' },
-  verification: { modelName: 'verificationTokens' },
+  user: { modelName: "users" },
+  session: { modelName: "sessions" },
+  account: { modelName: "accounts" },
+  verification: { modelName: "verificationTokens" },
   emailAndPassword: {
-    enabled: true
+    enabled: true,
   },
   plugins: [
     username(),
@@ -26,20 +26,20 @@ export const auth = betterAuth({
       defaultRole: UserRoles.USER,
       adminRole: [UserRoles.SUPER_ADMIN, UserRoles.ADMIN],
       impersonationSessionDuration: 60 * 60, // 1 hour
-      defaultBanExpiresIn: 60 * 60 * 24 * 7 // 7 days
+      defaultBanExpiresIn: 60 * 60 * 24 * 7, // 7 days
     }),
     passkey({
       rpID: process.env.WEB_BASE_URL,
-      rpName: 'flow',
-      origin: process.env.WEB_BASE_URL
-    })
+      rpName: "augment",
+      origin: process.env.WEB_BASE_URL,
+    }),
   ],
   advanced: {
-    generateId: false
-  }
+    generateId: false,
+  },
 });
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  plugins: [usernameClient(), passkeyClient()]
+  plugins: [usernameClient(), passkeyClient()],
 });
